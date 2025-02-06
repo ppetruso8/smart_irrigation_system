ee0b563039ab81a12613485c27710ce904262cf9
Petra Petrusova <petrusovap03@gmail.com>
2025-02-06 15:09:49 +0000

    Weather API initial

--- webapp/app.py
+++ webapp/app.py
@@ -4,6 +4,8 @@ from flask_session import Session
 # from werkzeug.security import generate_password_hash, check_password_hash
 # from forms import
 from functools import wraps
+import openmeteo_requests
+from openmeteo_sdk.Variable import Variable
 import time
 
 app = Flask(__name__)
@@ -13,13 +15,26 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"   
 Session(app)
 
+
+# initilaze weather API client
+om = openmeteo_requests.Client()
+# create caching session ?
+
 # @app.before_request
 # def load_logged_in_user():
 #     g.user = session.get("user_id", None)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # https://pypi.org/project/openmeteo-requests/
+    # API info MAKE CUSTOMIZABLE FROM DASHBOARD
+    longitude = -8.4706
+    latitude = 51.898
+
+    weather_data = get_weather_api(latitude, longitude)
+
+
+    return render_template("index.html", weather=weather_data)
 
 # @app.route("/register", methods = ["GET", "POST"])
 # def register():
@@ -88,3 +103,36 @@ def profile():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("error.html", error=error)
+
+
+def get_weather_api(latitude,longitude):
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "hourly": ["temperature_2m", "precipitation", "wind_speed_10m"],
+        "current": ["temperature_2m", "relative_humidity_2m"]
+    }
+
+    responses = om.weather_api("https://api.open-meteo.com/v1/forecast", params=params)
+    # weather = responses[0]["current_weather"]
+    
+    response = responses[0]
+    # print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    # print(f"Elevation {response.Elevation()} m asl")
+    # print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    # print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+    # Current values
+    current = response.Current()
+    current_variables = list(map(lambda i: current.Variables(i), range(0, current.VariablesLength())))
+    current_temperature_2m = next(filter(lambda x: x.Variable() == Variable.temperature and x.Altitude() == 2, current_variables))
+    current_relative_humidity_2m = next(filter(lambda x: x.Variable() == Variable.relative_humidity and x.Altitude() == 2, current_variables))
+
+    # print(f"Current time {current.Time()}")
+    # print(f"Current temperature_2m {current_temperature_2m.Value()}")
+    # print(f"Current relative_humidity_2m {current_relative_humidity_2m.Value()}")
+
+    return {
+        "temperature": current_temperature_2m.Value(),
+        "humidity": current_relative_humidity_2m.Value()
+    }
