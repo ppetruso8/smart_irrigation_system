9fcd74091d92a8e36c76b8d37cc8cfbfe89a3fbc
Petra Petrusova <petrusovap03@gmail.com>
2025-03-09 20:32:58 +0000

    Added auto-scroll for hourly forecast

---
+++ webapp/static/script.js
@@ -0,0 +1,32 @@
+/* Scroll to correct item in hourly forecast based on current time */
+let now = new Date();
+let curTime;
+let hourlyItem;
+let hourlyContainer = document.getElementById('hourly_container');
+
+document.addEventListener("DOMContentLoaded", init, false);
+
+function init() {
+    // get current time and transform to format as in HTML
+    now.setMinutes(0);  // round current time down to nearest hour 
+    curTime = now.toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false
+    }).replace(",", "");
+
+    // find an item in HTML
+    hourlyItem = Array.from(document.querySelectorAll('.hourly_item')).find(el => el.querySelector('.time')?.textContent.trim() === curTime);
+
+    if (hourlyItem) {
+        hourlyContainer.scrollTo({
+            left: hourlyItem.offsetLeft - hourlyContainer.offsetLeft,
+            behavior: "smooth"
+        });
+    } else {
+        console.log("Error finding hourly item for current time:", currentTime);
+    }
+}
